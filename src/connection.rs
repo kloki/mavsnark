b2044b61062e26b89ac285faa9cbdac99d0e8c22
Koen Klinkers <k.klinkers@pm.me>
2026-02-22 09:40:08 +0100

    clippy

--- src/connection.rs
+++ src/connection.rs
@@ -3,8 +3,8 @@ use std::io;
 use mavlink::{MavConnection, common::MavMessage};
 
 pub fn connect(uri: &str) -> io::Result<Box<dyn MavConnection<MavMessage> + Send + Sync>> {
-    let mut connection = mavlink::connect::<MavMessage>(uri)
-        .map_err(|e| io::Error::other(format!("{uri}: {e}")))?;
+    let mut connection =
+        mavlink::connect::<MavMessage>(uri).map_err(|e| io::Error::other(format!("{uri}: {e}")))?;
 
     connection.set_protocol_version(mavlink::MavlinkVersion::V2);
 
